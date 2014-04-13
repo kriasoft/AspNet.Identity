4ad544a4aba67e89f17896c2adfeb19a0f742f08
Konstantin Tarkus <hello@tarkus.me>
2014-04-13 14:12:56 +0400

    Add post deployment scripts with a reference data

--- examples/KriaSoft.AspNet.Identity.DbFirst/Startup.cs
+++ examples/KriaSoft.AspNet.Identity.DbFirst/Startup.cs
@@ -8,25 +8,30 @@ using Owin;
 
 namespace KriaSoft.AspNet.Identity.DbFirst
 {
+    using System.Linq;
+    using System.Text;
+
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             var db = new ApplicationDbContext();
             var userManager = new UserManager<User, int>(new UserStore(db));
-            var user = userManager.FindByNameAsync("demouser").Result;
 
-            if (user == null)
+            var sb = new StringBuilder();
+            var format = "{0, -3} | {1, -15} | {2, -30}";
+            sb.AppendLine(string.Format(format, "ID", "Username", "Email"));
+            sb.AppendLine("------------------------------------------------------");
+
+            foreach (var user in userManager.Users)
             {
-                var result = userManager.CreateAsync(new User { UserName = "demouser" }, "Passw0rd").Result;
-                user = userManager.FindAsync("demouser", "Passw0rd").Result;
-                System.Diagnostics.Debug.Assert(user != null && user.UserName == "demouser");
+                sb.AppendLine(string.Format(format, user.Id, user.UserName, user.Email));
             }
 
             app.Run(context =>
             {
                 context.Response.ContentType = "text/plain";
-                return context.Response.WriteAsync("Entity Framework Database-First Provider for ASP.NET Identity");
+                return context.Response.WriteAsync("ASP.NET Identity Users: \n\n" + sb.ToString());
             });
         }
     }
